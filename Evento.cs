1a5876851ae9e4cb8605e4eafe298d0346e14e6e
Fabio-F16 <fipsfabio95@gmail.com>
2022-07-06 17:39:21 +0200

    Milestone 1

---
+++ Evento.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Evento
+    {
+
+        // attributi
+        private string titolo;
+        private DateOnly date;
+        private int capienzaMassima;
+        public int postiPrenotati { get; private set; }
+
+        public string Titolo { 
+            get{
+                return this.titolo;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("Il titolo non può essere vuoto");
+                }
+
+                this.titolo = value;
+            }
+        }
+        public DateOnly Date { 
+            get{
+               
+                return this.date;
+            }
+            set
+            {
+                if(value < DateOnly.FromDateTime(DateTime.Now))
+                {
+                    throw new ArgumentException("La data è passata");
+                }
+                this.date = value;
+            }
+        }
+        public int CapienzaMassima { 
+            get{               
+                return this.capienzaMassima;    
+            }
+            protected set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("La capienza non può essere negativa");
+                }
+                this.capienzaMassima = value;
+            }
+        }
+        
+        
+
+        // costruttore
+        public Evento(string titolo, DateOnly date, int capienzaMassima)
+        {
+            this.Titolo = titolo;
+            this.Date = date;
+            this.CapienzaMassima = capienzaMassima;
+        }
+
+
+        // metodi
+        public int PrenotaPosti(int numeroPostiDaPrenotare)
+        {
+            
+            if(this.postiPrenotati > this.getPostiDisponibili())
+            {
+                throw new ArgumentNullException("Posti esauriti");
+            }
+            this.postiPrenotati += numeroPostiDaPrenotare;
+            return this.postiPrenotati;
+        }
+
+        public int DisdiciPosti(int numeroPostiDaPrenotare)
+        {
+            this.postiPrenotati -= numeroPostiDaPrenotare;
+            return this.postiPrenotati;
+        }
+
+        public int getPostiDisponibili()
+        {
+            int postiDisponibili = 0;
+            postiDisponibili = this.CapienzaMassima - this.postiPrenotati;
+            return postiDisponibili;
+        }
+
+
+        public void ToString()
+        {
+            Console.WriteLine("Il nome dell'evento è: " + this.Titolo);
+            Console.WriteLine("La data dell'evento è: " + this.Date);
+            Console.WriteLine("La capienza massima dell'evento è: " + this.capienzaMassima);
+
+           // return base.ToString() + "\n" + "Il nome dell'evento è: " + this.Titolo + "\n" + "La data dell'evento è: " + this.Date + "La capienza massima dell'evento è: " +  this.capienzaMassima;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace csharp_gestore_eventi
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            Evento evento = new Evento("hvv", DateOnly.FromDateTime(DateTime.MaxValue), 1000);
+            evento.ToString();
+            
         }
     }
 }
